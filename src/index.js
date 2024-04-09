32cb9fe58437088014879bba423f82e2b7950dd1
Amir Abdur-Rahim <amirabdurrahim@gmail.com>
2024-04-09 14:26:50 -0500

    Added new project button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./style.css";
+import Todo from "./todoItem.js";
+import Project from "./project.js";
+
+const projectBtn = document.createElement("button");
+projectBtn.textContent = "New Project";
+document.body.appendChild(projectBtn);
