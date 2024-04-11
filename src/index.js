9708d751ea9ddd1e87613ed20ac621cb3e728193
Amir Abdur-Rahim <amirabdurrahim@gmail.com>
2024-04-11 09:16:06 -0500

    Started writing new project function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,23 @@ import "./style.css";
 import Todo from "./todoItem.js";
 import Project from "./project.js";
 
-const projectBtn = document.createElement("button");
-projectBtn.textContent = "New Project";
-document.body.appendChild(projectBtn);
+const newProjectWrapper = document.createElement("div");
+newProjectWrapper.classList.add("new-project");
+document.body.appendChild(newProjectWrapper);
+
+const newProject = document.createElement("input");
+newProject.type = "text";
+newProject.id = "projectName";
+newProject.placeholder = "New Project";
+newProjectWrapper.appendChild(newProject);
+
+const newProjectBtn = document.createElement("button");
+newProjectBtn.textContent = "Add new project";
+newProjectBtn.addEventListener("click", createProject);
+newProjectWrapper.appendChild(newProjectBtn);
+
+function createProject() {
+  const project = new Project(`${newProject.value}`);
+  console.log(project);
+  newProject.value = "";
+}
--- src/todoItem.js
+++ src/todoItem.js
@@ -8,3 +8,32 @@ export default class TodoItem {
     this.checkList = checkList;
   }
 }
+
+const addTodo = document.createElement("button");
+addTodo.classList.add("add-todo-btn");
+addTodo.textContent = "Add todo";
+addTodo.addEventListener("click", createTodo);
+document.body.appendChild(newTodo);
+
+function newTodo() {
+  const newTodoWrapper = document.createElement("div");
+  newTodoWrapper.classList.add("new-todo-wrapper");
+  document.body.appendChild(newTodoWrapper);
+
+  const newTodoTitle = document.createElement("input");
+  newTodoTitle.id = "new-todo-title";
+  newTodoTitle.placeholder = "New todo";
+  newTodoWrapper.appendChild(newTodoTitle);
+
+  const newTodoDescription = document.createElement("input");
+  newTodoDescription.id = "new-todo-description";
+  newTodoDescription.placeholder = "New todo";
+  newTodoWrapper.appendChild(newTodoDescription);
+}
+
+function addTodoItem() {
+  const todo = new TodoItem(
+    `${newTodoTitle.value}`,
+    `${newTodoDescription.value}`
+  );
+}
