836c6f577938d026147177cd87d97b2cc73a221f
Amir Abdur-Rahim <amirabdurrahim@gmail.com>
2024-04-12 03:46:39 -0500

    Added sidebar displaying projects + addProjectBtn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import header from "./header.js";
 // import Todo from "./todoItem.js";
-import Project from "./project.js";
+import Projects from "./project.js";
 
 document.body.appendChild(header());
 
@@ -9,6 +9,4 @@ const mainWrapper = document.createElement("div");
 mainWrapper.classList.add("main-wrapper");
 document.body.appendChild(mainWrapper);
 
-const newProjectWrapper = document.createElement("div");
-newProjectWrapper.classList.add("new-project");
-mainWrapper.appendChild(newProjectWrapper);
+mainWrapper.appendChild(Projects());
--- src/project.js
+++ src/project.js
@@ -1,4 +1,4 @@
-export default class Project {
+class Project {
   constructor(name) {
     this.name = name;
     this.todos = [];
@@ -9,21 +9,57 @@ export default class Project {
   }
 }
 
-const projects = [];
+const projectsList = [];
 
 function addProject(name) {
   const newProject = new Project(name);
-  projects.push(newProject);
+  projectsList.push(newProject);
   displayProjects();
 }
 
-const newProject = document.createElement("input");
-newProject.type = "text";
-newProject.id = "projectName";
-newProject.placeholder = "New Project";
-newProjectWrapper.appendChild(newProject);
+function displayProjects() {
+  const projectsContainer = document.querySelector("#project-list-wrapper");
+  projectsContainer.innerHTML = ""; // Clear the container before displaying updated list
 
-const newProjectBtn = document.createElement("button");
-newProjectBtn.textContent = "Add new project";
-newProjectBtn.addEventListener("click", createProject);
-newProjectWrapper.appendChild(newProjectBtn);
+  projectsList.forEach((project) => {
+    const projectElement = document.createElement("div");
+    const projectName = document.createElement("h2");
+    projectName.textContent = project.name;
+    projectElement.appendChild(projectName);
+
+    project.todos.forEach((todo) => {
+      const todoElement = document.createElement("p");
+      todoElement.textContent = todo;
+      projectElement.appendChild(todoElement);
+    });
+
+    projectsContainer.appendChild(projectElement);
+  });
+}
+
+export default function Projects() {
+  const projectWrapper = document.createElement("div");
+  projectWrapper.classList.add("project-wrapper");
+  projectWrapper.id = "project-wrapper";
+
+  const newProject = document.createElement("input");
+  newProject.type = "text";
+  newProject.id = "projectName";
+  newProject.placeholder = "New Project";
+  projectWrapper.appendChild(newProject);
+
+  const newProjectBtn = document.createElement("button");
+  newProjectBtn.textContent = "Add new project";
+  newProjectBtn.addEventListener("click", () => {
+    addProject(`${newProject.value}`);
+    newProject.value = "";
+  });
+  projectWrapper.appendChild(newProjectBtn);
+
+  const projectListWrapper = document.createElement("div");
+  projectListWrapper.classList.add("project-list-wrapper");
+  projectListWrapper.id = "project-list-wrapper";
+  projectWrapper.appendChild(projectListWrapper);
+
+  return projectWrapper;
+}
