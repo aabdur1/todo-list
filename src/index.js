d62a1d505c0f38861908fb5a1baaa3d74771ae7e
Amir Abdur-Rahim <amirabdurrahim@gmail.com>
2024-04-12 02:21:18 -0500

    Moved new project btn to project.js Created addProject function to add and display projects

--- src/index.js
+++ src/index.js
@@ -5,23 +5,10 @@ import Project from "./project.js";
 
 document.body.appendChild(header());
 
+const mainWrapper = document.createElement("div");
+mainWrapper.classList.add("main-wrapper");
+document.body.appendChild(mainWrapper);
+
 const newProjectWrapper = document.createElement("div");
 newProjectWrapper.classList.add("new-project");
-document.body.appendChild(newProjectWrapper);
-
-const newProject = document.createElement("input");
-newProject.type = "text";
-newProject.id = "projectName";
-newProject.placeholder = "New Project";
-newProjectWrapper.appendChild(newProject);
-
-const newProjectBtn = document.createElement("button");
-newProjectBtn.textContent = "Add new project";
-newProjectBtn.addEventListener("click", createProject);
-newProjectWrapper.appendChild(newProjectBtn);
-
-function createProject() {
-  const project = new Project(`${newProject.value}`);
-  console.log(project);
-  newProject.value = "";
-}
+mainWrapper.appendChild(newProjectWrapper);
--- src/project.js
+++ src/project.js
@@ -8,3 +8,22 @@ export default class Project {
     this.todos.push(todo);
   }
 }
+
+const projects = [];
+
+function addProject(name) {
+  const newProject = new Project(name);
+  projects.push(newProject);
+  displayProjects();
+}
+
+const newProject = document.createElement("input");
+newProject.type = "text";
+newProject.id = "projectName";
+newProject.placeholder = "New Project";
+newProjectWrapper.appendChild(newProject);
+
+const newProjectBtn = document.createElement("button");
+newProjectBtn.textContent = "Add new project";
+newProjectBtn.addEventListener("click", createProject);
+newProjectWrapper.appendChild(newProjectBtn);
